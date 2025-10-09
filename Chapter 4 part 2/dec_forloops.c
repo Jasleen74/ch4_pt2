e8c00b7f2e9d6c83606e7f028f758386fb19cb27
Jasleen74 <preetjee@gmail.com>
2025-10-09 13:47:33 +0800

    (the decrement in for loops)

---
+++ Chapter 4 part 2/dec_forloops.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+     /* we can also write the code as
+     int i ;
+     for(i =5; i ) and so on it is upto us */
+    for(int i = 5 ; i ; i -- )   
+    {                                       // Here also we dont need to type {}always when we have only a single line of code
+        printf("%d is the value of i \n",i);
+    }
+  
+ return 0;
+}   // this code will print the values until zero
