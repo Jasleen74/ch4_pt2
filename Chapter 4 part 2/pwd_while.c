9d858b4f6ac96db73d76a241d53d2fdcf20e672f
Jasleen74 <preetjee@gmail.com>
2025-10-09 11:59:16 +0800

    (the pwd question using just the while)

---
+++ Chapter 4 part 2/pwd_while.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() {
+     int pwd;
+
+     while (pwd != 1234)
+     {
+         printf("Enter ur pwd : ");
+     scanf("%d",& pwd);
+     
+     if (pwd !=1234)
+        printf ("renetr the pwd \n");
+     }
+     printf("the pwd is correct ");
+  
+ return 0;
+}                                               
