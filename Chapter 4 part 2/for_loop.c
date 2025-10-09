5abd08cb6140449725beb5a89d3d455e37f5cd68
Jasleen74 <preetjee@gmail.com>
2025-10-09 12:33:55 +0800

    (for loop in c )

---
+++ Chapter 4 part 2/for_loop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+     int n ;
+     printf("pls enter the value of n : ");
+     scanf("%d",&n);
+
+     for(int i = 4 ; i < n ; i ++ )
+     {
+        printf("the value of i is %d\n " ,i);
+     }
+  
+ return 0;
+}
